ea5621644dc659ced594ac91be0b083ba6fcde53
Wiyoga Ramadhan <wiyogaramadhan1@gmail.com>
2024-02-13 13:53:38 +0700

    anime menu javascript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika animemenu di klik
+document.querySelector("#animemenu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
